6d2c716a10f9dc8c7ed9f35bba8ab7dfd32f326a
Daniel Vargas <daniellopez882017@gmail.com>
2024-04-19 21:59:36 -0600

    Se ha implementado la recursividad en la funcion ActivarEnFechaYHoraEspecificos para llamar la funcion cada mes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Qr = require('qrcode-terminal');
 
 //CONSTANTES PARA CONECTAR CON WHATSAPP WEB
-const { Client, LocalAuth} = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({ //cliente
     authStrategy: new LocalAuth(),
     /*   puppeteer: {
@@ -10,19 +10,19 @@ const client = new Client({ //cliente
             headless: true,
     },
         }, */
-        webVersion: '2.2409.2',
-        webVersionCache: {
-            type: 'remote',
-            remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.2.html'
-        }
-    });
-    
+    webVersion: '2.2409.2',
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.2.html'
+    }
+});
+
 
 
 //PRUEBA DE OBTENCIOMN DE DATOS
 // archivo_principal.js
 const { obtenerPagosDelAlumno } = require('./respuestasApi.js'); // Importar la función desde el archivo
-const {obtenerArrayDeAlumnosSinPagoEsteMes} = require('./datosDePagos.js')
+const { obtenerArrayDeAlumnosSinPagoEsteMes } = require('./datosDePagos.js')
 //GENERAR QR EN TERMINAL
 client.on('qr', (qr) => {
     Qr.generate(qr, { small: true });
@@ -50,7 +50,7 @@ client.on('message', (message) => {
 client.on('message', async (message) => {
     try {
         const pagos_alumno = await obtenerPagosDelAlumno(message.body);
-        
+
         //TEXTO FINAL A IMPRIMIR
         let texto = `Datos del alumno:
         Apellido: ${pagos_alumno.apellido1}
@@ -78,7 +78,7 @@ client.initialize();
 
 //UTILIZA EL CLIENTE PARA ENVIAR UN MENSAJE DE ACUERDO A LOS PARAMETROS
 async function enviarMensaje(numeroDeTelefono, textoDelMensaje) {
-    const numero = numeroDeTelefono; 
+    const numero = '52' + numeroDeTelefono; //AGREGA LA LADA DE MEXICO PARA PODER ENVIAR EL MENSAJE 
     const texto = textoDelMensaje;
 
     try {
@@ -88,18 +88,18 @@ async function enviarMensaje(numeroDeTelefono, textoDelMensaje) {
         console.error("Error al enviar el mensaje:", error);
     }
 }
-async function enviarMensajeALosAlumnosSinPago(){
+async function enviarMensajeALosAlumnosSinPago() {
     // Iterando sobre el array e imprimiendo los nombres de todas las personas
     const alumnosSinPago = await obtenerArrayDeAlumnosSinPagoEsteMes();
-    
+
     for (const alumno of alumnosSinPago) {
         console.log(alumno.nombre);
         const mensaje = "El alumno " + alumno.nombre + " no ha realizado su pago este mes";
 
-        // Crear una promesa que se resolverá después de 3 minutos
+        // Crear una promesa que se resolverá después de 3 minutos y luego envia un numero al telefono del alumno
         await new Promise(resolve => {
             setTimeout(async () => {
-                await enviarMensaje('524471389071', mensaje);
+                await enviarMensaje(alumno.telefono, mensaje);
                 resolve(); // Resuelve la promesa después de enviar el mensaje
             }, 180000); // 180000 milisegundos = 3 minutos
         });
@@ -108,24 +108,40 @@ async function enviarMensajeALosAlumnosSinPago(){
 
 
 // Función asíncrona para activar en un día y hora específicos
-async function activarEnDiaYHoraEspecificos() {
+async function activarEnDiaYHoraEspecificos(fecha) {
     // Definir la fecha y hora específicas en las que quieres activar la función
-    const fechaEspecifica = new Date(2024, 3, 3, 19, 49, 0); 
+    const fechaEspecifica = fecha
+    // Definir la fecha específica
+
+
+
+    // Llamar a la función para avanzar un mes
+
 
     while (true) {
         // Obtener la fecha y hora actual
         const ahora = new Date();
-        
+
         // Verificar si la fecha y hora actuales coinciden con la fecha y hora específicas
         if (ahora >= fechaEspecifica) {
             console.log("La función se activó en la fecha y hora específicas.");
             await enviarMensajeALosAlumnosSinPago();
+            // Función para avanzar un mes
+           async function avanzarUnMes() {
+                fechaEspecifica.setMonth(fechaEspecifica.getMonth() + 1);
+                console.log('Fecha actualizada:', fechaEspecifica);
+            }
+
+           await avanzarUnMes();
+            //VOLVIENDO A LLAMAR A LA FUNCION
+            activarEnDiaYHoraEspecificos(fechaEspecifica)
+                .catch(error => console.error("Error:", error));
             break; // Salir del bucle una vez que se haya activado la función
         } else {
             // Calcular el tiempo que falta hasta la fecha y hora específicas
             const tiempoRestante = fechaEspecifica.getTime() - ahora.getTime();
             console.log(`Esperando hasta la fecha y hora específicas (${tiempoRestante} ms restantes)...`);
-            
+
             // Esperar un tiempo antes de volver a verificar
             await esperar(1000); // Esperar 1 segundo antes de volver a verificar
         }
@@ -138,7 +154,8 @@ function esperar(ms) {
 }
 
 // Llamar a la función para activar en un día y hora específicos
-activarEnDiaYHoraEspecificos()
+const fechaEspecifica = new Date(2024, 3, 19, 21, 42, 0);
+activarEnDiaYHoraEspecificos(fechaEspecifica)
     .catch(error => console.error("Error:", error));
 
 
