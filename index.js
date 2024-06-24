f7771ae72dd1923f52166aa24788e1c22e98c80b
Daniel Vargas <daniellopez882017@gmail.com>
2024-06-24 13:34:13 -0600

    Cambiando estructura para que escuche un puerto y asi render lo acepte

--- index.js
+++ index.js
@@ -1,87 +1,61 @@
-//CONSTANTE PARA GENERAR UN CODIGO QR EN LA TERMINAL
+// Importa los módulos necesarios
+const express = require('express');
 const Qr = require('qrcode-terminal');
-
-//CONSTANTES PARA CONECTAR CON WHATSAPP WEB
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const client = new Client({ //cliente
-    authStrategy: new LocalAuth(),
-    /*   puppeteer: {
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            headless: true,
-    },
-        }, */
-    /* webVersion: '2.2409.2', */
-    /* webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.2.html'
-    } */
-});
-
-
-
-//PRUEBA DE OBTENCIOMN DE DATOS
-// archivo_principal.js
-const { obtenerPagosDelAlumno } = require('./respuestasApi.js'); // Importar la función desde el archivo
+const { obtenerPagosDelAlumno } = require('./respuestasApi.js');
 const { obtenerArrayDeAlumnosSinPagoEsteMes } = require('./datosDePagos.js');
 const logger = require('./logger.js');
-//GENERAR QR EN TERMINAL
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Configura el cliente de WhatsApp Web
+const client = new Client({
+    authStrategy: new LocalAuth(),
+});
+
+// Genera un QR en la terminal para iniciar sesión
 client.on('qr', (qr) => {
     Qr.generate(qr, { small: true });
 });
 
-//CUANDO EL CLIENTE ESTA LISTO
+// Cuando el cliente está listo
 client.on('ready', async () => {
     logger.info('Client is ready!');
-    //LO COMENTO PARA DESCOMENTAR DESPUES Y NO MOLESTAR
-    /* const number = "524471389071"; 
-    const text = "Hey I'm ready!";
-    const phoneRegistered = await client.getNumberId(number);
-    await client.sendMessage(phoneRegistered._serialized, text);
-    */
 });
 
-//Cuando el cliente recibe un mensaje, este se muestra en la consola
-client.on('message', (message) => {
-    console.log(message.body);
-});
-
-//Si el encuentra un alumno envia un mensaje con los datos
-// Archivo principal (por ejemplo, index.js)
-
+// Escucha los mensajes y responde
 client.on('message', async (message) => {
     try {
         const pagos_alumno = await obtenerPagosDelAlumno(message.body);
-
-        //TEXTO FINAL A IMPRIMIR
-        let texto = `Datos del alumno:
-        Apellido: ${pagos_alumno.apellido1}
-        Nombre: ${pagos_alumno.nombre}
-        Pagos realizados:`;
-
-        //Concatena cada uno de los datos del pago
+        let texto = `Datos del alumno:\nApellido: ${pagos_alumno.apellido1}\nNombre: ${pagos_alumno.nombre}\nPagos realizados:`;
         pagos_alumno.pagos.forEach((pago, index) => {
-            texto += `
-            - Pago ${index + 1}:
-                Estado: ${pago.estado_pago}
-                Fecha: ${pago.fecha_pago}
-                Monto: ${pago.monto}`;
+            texto += `\n- Pago ${index + 1}:\n  Estado: ${pago.estado_pago}\n  Fecha: ${pago.fecha_pago}\n  Monto: ${pago.monto}`;
         });
-
         await message.reply(`Hola ${pagos_alumno.nombre}, aquí están tus datos:\n${texto}`);
     } catch (error) {
-        logger.error(error.message); // Manejar cualquier error que ocurra al buscar el alumno
+        logger.error(error.message);
         await message.reply('Ha ocurrido un error buscando tus datos. Por favor, inténtalo de nuevo más tarde.');
     }
 });
 
-
+// Inicializa el cliente de WhatsApp Web
 client.initialize();
 
-//UTILIZA EL CLIENTE PARA ENVIAR UN MENSAJE DE ACUERDO A LOS PARAMETROS
-async function enviarMensaje(numeroDeTelefono, textoDelMensaje) {
-    const numero = '52' + numeroDeTelefono; //AGREGA LA LADA DE MEXICO PARA PODER ENVIAR EL MENSAJE 
-    const texto = textoDelMensaje;
+// Endpoint para mantener el puerto abierto
+app.get('/', (req, res) => {
+    res.send('Service is up and running');
+});
 
+// Servidor HTTP que mantiene el puerto abierto
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+// Lógica de envío de mensajes (sin cambios)
+async function enviarMensaje(numeroDeTelefono, textoDelMensaje) {
+    const numero = '52' + numeroDeTelefono;
+    const texto = textoDelMensaje;
     try {
         const phoneRegistered = await client.getNumberId(numero);
         await client.sendMessage(phoneRegistered._serialized, texto);
@@ -90,10 +64,7 @@ async function enviarMensaje(numeroDeTelefono, textoDelMensaje) {
     }
 }
 
-const MILISEGUNDOS_EN_UN_DIA = 24 * 60 * 60 * 1000; // 24 horas en milisegundos
-const MILISEGUNDOS_EN_UN_MINUTO = 60 * 1000; // 60 segundos en milisegundos
-
-// Función para obtener y enviar mensajes a los alumnos sin pago
+// Función principal para enviar mensajes a alumnos sin pago
 async function enviarMensajeALosAlumnosSinPago() {
     const alumnosSinPago = await obtenerArrayDeAlumnosSinPagoEsteMes();
     if (!alumnosSinPago || alumnosSinPago.length === 0) {
@@ -102,71 +73,38 @@ async function enviarMensajeALosAlumnosSinPago() {
         logger.info(`Se encontraron ${alumnosSinPago.length} alumnos sin pago este mes.`);
         for (const alumno of alumnosSinPago) {
             logger.info(`Enviando mensaje a: ${alumno.nombre}`);
-            const mensaje = `Hola ${alumno.nombre}, esperando que se encuentre muy bien, le saluda la Preparatoria Federal por Coperación. ` +
-                `Le invitamos a realizar el pago correspondiente de este mes. Si ya realizó su pago, por favor infórmelo a nuestras oficinas. ¡Saludos!`;
-
-            // Esperar 3 minutos antes de enviar el mensaje
-            await esperar(180000); // 180000 milisegundos = 3 minutos
+            const mensaje = `Hola ${alumno.nombre}, esperando que se encuentre muy bien le saluda Preparatoria Federal por Coperación. Le invitamos a realizar el pago correspondiente de este mes. Si ya realizó su pago, por favor infórmelo a nuestras oficinas. ¡Saludos!`;
+            await esperar(180000); // Espera 3 minutos
             await enviarMensaje(alumno.telefono, mensaje);
         }
     }
 }
 
-// Función principal para activar en un día y hora específicos
-/* async function activarEnDiaYHoraEspecificos(fechaInicial) {
-    let fechaProximaActivacion = fechaInicial;
-
-    while (true) {
-        const ahora = new Date();
-
-        if (ahora >= fechaProximaActivacion) {
-            logger.info("La función se activó en la fecha y hora específicas.");
-            await enviarMensajeALosAlumnosSinPago();
-            await avanzarUnMes(fechaProximaActivacion);
-            fechaProximaActivacion.setMonth(fechaProximaActivacion.getMonth() + 1);
-            logger.info(`Fecha actualizada para la próxima activación: ${fechaProximaActivacion}`);
-        }
-
-        const tiempoRestante = fechaProximaActivacion.getTime() - ahora.getTime();
-        logger.info(`Esperando hasta la próxima fecha y hora específicas (${tiempoRestante} ms restantes)...`);
-        await esperar(Math.min(tiempoRestante, MILISEGUNDOS_EN_UN_MINUTO)); // Esperar hasta la próxima fecha o 1 minuto, lo que sea menor
-    }
-}
- */
+// Función para activar en una fecha específica
 async function activarEnDiaYHoraEspecificos(fecha) {
     while (true) {
         const ahora = new Date();
-        if (ahora >= fecha){
-            logger.info("La funcion se activo en la fecha y hora específicas")
-            //enviar mensaje a los alumnos
+        if (ahora >= fecha) {
+            logger.info("La función se activó en la fecha y hora específicas.");
             await enviarMensajeALosAlumnosSinPago();
-            //Hacer avanzar la fecha un dia
             await avanzarUnDia(fecha);
-            //Esperar otro dia antes de volver a comprobar
-            await esperar(MILISEGUNDOS_EN_UN_DIA);
+            await esperar(24 * 60 * 60 * 1000); // Espera un día
         } else {
             const tiempoRestante = fecha.getTime() - ahora.getTime();
-            logger.info("Esperando hasta la proxima fecha o un minuto, lo que sea menor")
-            await esperar(Math.min(tiempoRestante, 60 * 1000))
+            await esperar(Math.min(tiempoRestante, 60 * 1000)); // Espera hasta la fecha o un minuto
         }
     }
 }
-// Función para avanzar la fecha al próximo mes
-/* async function avanzarUnMes(fecha) {
-    fecha.setMonth(fecha.getMonth() + 1);
-    logger.info('Fecha actualizada al próximo mes: ' + fecha);
-} */
-async function avanzarUnDia(fecha){
-    fecha.setDate(fecha.getDate() + 1)
-    logger.info("Fecha actualizada al proximo dia " +  fecha);
+
+async function avanzarUnDia(fecha) {
+    fecha.setDate(fecha.getDate() + 1);
+    logger.info("Fecha actualizada al próximo día: " + fecha);
 }
 
-// Función auxiliar para esperar
 function esperar(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Inicializar el proceso de activación
-const fechaInicial = new Date(2024, 5, 24, 10, 1, 0); // Ejemplo: 19 de junio de 2024 a las 21:42:00
-activarEnDiaYHoraEspecificos(fechaInicial)
-    .catch(error => logger.error("Error:" + error));
+// Inicia la activación en una fecha específica
+const fechaInicial = new Date(2024, 5, 24, 10, 1, 0);
+activarEnDiaYHoraEspecificos(fechaInicial).catch(error => logger.error("Error:" + error));
